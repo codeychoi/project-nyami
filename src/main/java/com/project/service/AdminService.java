37bb2f489975eec36b93497c1f19971f929d6dd8
NAMEJM-sys <dlwjdals0803@gmail.com>
2024-11-11 15:14:22 +0900

    관리자 페이지 공지사항 승인 폼 추가 확인
    동작 확인 완료 승인 폼 출력 완

--- src/main/java/com/project/service/AdminService.java
+++ src/main/java/com/project/service/AdminService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.project.domain.Member;
-import com.project.domain.Notice;
 import com.project.domain.Review;
 import com.project.domain.Store;
+import com.project.dto.Pagination;
+import com.project.dto.RequestData;
 import com.project.domain.Menu;
+import com.project.domain.Notice;
 import com.project.mapper.MemberMapper;
 import com.project.mapper.MenuMapper;
 import com.project.mapper.NoticeMapper;
@@ -24,7 +26,7 @@ public class AdminService {
 	private final MemberMapper memberMapper;
 	private final StoreMapper storeMapper;
 	private final MenuMapper menuMapper;
-	private final ReviewMapper reviewMapper;  
+	private final ReviewMapper reviewMapper;
 
 	// 공지 작성
 	public void insertNotice(Notice notice) {
@@ -32,11 +34,17 @@ public class AdminService {
 	}
 	
 	// 유저 조회
-	public List<Member> selectMembers(int page, int limit) {
+	public Pagination<Member> selectMembers(RequestData requestData) {
+		int page = requestData.getPage();
+		int limit = requestData.getLimit();
+		
 		int start = (page - 1) * limit + 1;
 		int end = start + limit - 1;
+		long totalCount = memberMapper.countMembers();
 		
-		return memberMapper.selectMembers(start, end);
+		List<Member> members = memberMapper.selectMembers(start, end);
+		
+		return new Pagination<>(members, page, limit, totalCount);
 	}
 	
 	// 특정 유저 조회
@@ -44,18 +52,46 @@ public class AdminService {
 		return memberMapper.selectMember(id);
 	}
 
-	/*
-	 * // 가게 목록 조회 public List<Store> selectStores(int page, int limit) { int start
-	 * = (page - 1) * limit + 1; int end = start + limit - 1;
-	 * 
-	 * return storeMapper.selectStores(start, end); }
-	 * 
-	 * // 메뉴 조희 public List<Menu> selectMenus(long storeId) { return
-	 * menuMapper.selectMenus(storeId); }
-	 * 
-	 * // 리뷰 조회 public List<Review> selectReviews(int page, int limit) { int start =
-	 * (page - 1) * limit + 1; int end = start + limit - 1;
-	 * 
-	 * return reviewMapper.selectReviews(start, end); }
-	 */
+	// 회원 차단
+	public void blockMember(long id) {
+		memberMapper.blockMember(id);
+	}
+	
+	// 회원 차단해제
+	public void unblockMember(long id) {
+		memberMapper.unblockMember(id);
+	}
+	
+	// 가게 목록 조회
+	public Pagination<Store> selectStores(RequestData requestData) {
+		int page = requestData.getPage();
+		int limit = requestData.getLimit();
+		
+		int start = (page - 1) * limit + 1;
+		int end = start + limit - 1;
+		long totalCount = storeMapper.countStores();
+		
+		List<Store> stores = storeMapper.selectStores(start, end);
+		
+		return new Pagination<>(stores, page, limit, totalCount);
+	}
+
+	// 메뉴 조희
+	public List<Menu> selectMenus(long storeId) {
+		return menuMapper.selectMenus(storeId);
+	}
+
+	// 리뷰 조회
+	public Pagination<Review> selectReviews(RequestData requestData) {
+		int page = requestData.getPage();
+		int limit = requestData.getLimit();
+		
+		int start = (page - 1) * limit + 1;
+		int end = start + limit - 1;
+		long totalCount = reviewMapper.countReviews();
+		
+		List<Review> reviews = reviewMapper.selectReviews(start, end);
+		
+		return new Pagination<>(reviews, page, limit, totalCount);
+	}
 }
